597efd938ae774a7e55241bc273c69d150dfadf6
Chintan_06 <159480965+Chintansgowda@users.noreply.github.com>
2025-06-12 14:28:38 +0530

    Update shell2.sh

--- shell2.sh
+++ shell2.sh
@@ -1,1 +1,1 @@
-" hey thsi shell2 and modified in github ui and gets pushed local repo that is our system "
+echo " hey this is shell2 and modified in github ui and gets pushed local repo that is our system "
